2df2bf49ae9b9c1a6ac53ff6ab2d82e0ea2146d1
Ross <oblivion1214@hotmail.com>
2017-10-30 22:53:13 +0000

    Added scss and began styles

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@ var gulp = require("gulp");
 var gutil = require("gulp-util");
 var gulpConcat = require("gulp-concat");
 var rename = require('gulp-rename');
+var sass = require('gulp-sass');
 var uglify = require("gulp-uglify");
 var cleanCss = require("gulp-clean-css");
 
 //Sources
-var cssSources = ["./public/css/main.css", "./node_modules/bootstrap/dist/css/bootstrap.css"];
-var jsSources = ["./node_modules/jquery/dist/jquery.js", "./node_modules/bootstrap/dist/js/bootstrap.js", "./node_modules/angular/angular.js", "./node_modules/angular-route/angular-route.js", "./node_modules/chart.js/dist/Chart.min.js", "./models/*.js", "./public/js/app/**/*.js", "./public/js/app/*.js"];
+var sassSources = ["./public/scss/boostrap/bootstrap.scss", "./public/scss/site/site.scss"];
+var sassFiles = ["./public/scss/**/*.scss", "./public/scss/**/**/*.scss"];
+var jsSources = ["./node_modules/angular/angular.js", "./node_modules/angular-route/angular-route.js", "./node_modules/chart.js/dist/Chart.min.js", "./models/*.js", "./public/js/app/**/*.js", "./public/js/app/*.js"];
+
 
 gulp.task("generateSiteCss", function(){
-    gulp.src(cssSources)
-    .pipe(gulpConcat("site.min.css").on('error', gutil.log))
+    gulp.src(sassSources)
+    .pipe(sass().on('error', gutil.log))
     .pipe(cleanCss().on('error', gutil.log))
     .pipe(gulp.dest("./public/css/"));
 });
@@ -26,7 +29,7 @@ gulp.task("generateSiteJs", function(){
 });
 
 gulp.task("watch", function(){
-    gulp.watch(cssSources, ["generateSiteCss"]);
+    gulp.watch(sassSources, ["generateSiteCss"]);
     gulp.watch(jsSources, ["generateSiteJs"]);
 });
 
