61fa913160546f7a32e65b45bc3573cef4394737
Ross <oblivion1214@hotmail.com>
2017-11-06 23:54:04 +0000

    Added front end styles

--- gulpfile.js
+++ gulpfile.js
@@ -5,26 +5,31 @@ var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var uglify = require("gulp-uglify");
 var cleanCss = require("gulp-clean-css");
+var sourceMaps = require("gulp-sourcemaps");
 
 //Sources
-var sassSources = ["./public/scss/boostrap/bootstrap.scss", "./public/scss/site/site.scss"];
+var sassSources = ["./public/scss/site/site.scss"];
 var sassFiles = ["./public/scss/**/*.scss", "./public/scss/**/**/*.scss"];
 var jsSources = ["./node_modules/angular/angular.js", "./node_modules/angular-route/angular-route.js", "./node_modules/chart.js/dist/Chart.min.js", "./models/*.js", "./public/js/app/**/*.js", "./public/js/app/*.js"];
 
 
 gulp.task("generateSiteCss", function(){
     gulp.src(sassSources)
+    .pipe(sourceMaps.init({largeFile: true}))
     .pipe(sass().on('error', gutil.log))
     .pipe(cleanCss().on('error', gutil.log))
+    .pipe(sourceMaps.write("../maps"))
     .pipe(gulp.dest("./public/css/"));
 });
 
 gulp.task("generateSiteJs", function(){
     gulp.src(jsSources)
+    .pipe(sourceMaps.init({largeFile: true}))
     .pipe(gulpConcat("site.js").on('error', gutil.log))
     .pipe(gulp.dest("./public/js/"))
     .pipe(uglify().on('error', gutil.log))
     .pipe(rename({extname: '.min.js'}))
+    .pipe(sourceMaps.write("../maps"))
     .pipe(gulp.dest("./public/js/"));
 });
 
