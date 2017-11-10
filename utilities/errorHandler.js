7352dbea1a4444b1fe401b1930e923b55bb8d372
Ross <oblivion1214@hotmail.com>
2017-11-10 03:18:14 +0000

    Added bug fix for poll with empty fields and added styles

--- utilities/errorHandler.js
+++ utilities/errorHandler.js
@@ -1,14 +1,17 @@
 
-
-var errorHandler = function(err, req, res, next){
-    if(err){
-        if (!(res.statusCode === 403 || res.statusCode === 401)) {
-            res.send("You Must Login To Continue");
+var errorHandler = function(err, req, res, next) {
+        if (err) {
+            if (!(res.statusCode === 403 || res.statusCode === 401)) {
+                console.warn(res.statusCode);
+                res.status(401);
+                res.json({"message":"You Must Login To Continue"});
+            }
+            else {
+                console.warn(err);
+                res.status(500);
+                res.json({"message": err.message});
+            }
         }
-        console.warn(err);
-        res.status(500);
-        res.send(err.message);
-    }
 };
 
 module.exports = errorHandler;
